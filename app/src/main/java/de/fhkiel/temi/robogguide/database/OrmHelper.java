c2266a8521a6cca0016ce05bc7b7db334c8a8b38
yusuf.oeztemiz <yusuf.oeztemiz@student.fh-kiel.de>
2024-11-05 15:03:17 +0100

    ausprobieren

---
+++ app/src/main/java/de/fhkiel/temi/robogguide/database/OrmHelper.java
@@ -0,0 +1,8 @@
+package de.fhkiel.temi.robogguide.database;
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+public class OrmHelper{
+
+}
