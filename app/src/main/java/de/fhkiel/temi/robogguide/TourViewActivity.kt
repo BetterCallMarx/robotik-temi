ffee754215342df1dde9a12613f45ac9ab0d84e3
Yusuf <yusuf.oeztemiz@student.fh-kiel.de>
2024-11-15 13:43:50 +0100

    stuff

--- app/src/main/java/de/fhkiel/temi/robogguide/TourViewActivity.kt
+++ app/src/main/java/de/fhkiel/temi/robogguide/TourViewActivity.kt
@@ -29,23 +29,24 @@ import java.lang.Thread.sleep
 import kotlin.concurrent.thread
 import com.squareup.picasso.Picasso;
 
-class TourViewActivity(): AppCompatActivity(), OnRobotReadyListener, OnGoToLocationStatusChangedListener {
+class TourViewActivity() : AppCompatActivity(), OnRobotReadyListener,
+    OnGoToLocationStatusChangedListener {
 
     private lateinit var mTourManager: TourManager
     private lateinit var mRobot: Robot
     private var isShort: Boolean = false
     var isLong: Boolean = false
-    var isInd : Boolean = false
+    var isInd: Boolean = false
     var isUndetailed: Boolean = false
     var isDetailed: Boolean = false
     var selectedPlace: String = ""
     var indexP: Int = 0
-    private lateinit var selectedLocations : MutableList<Location>
+    private lateinit var selectedLocations: MutableList<Location>
     private lateinit var locationsText: TextView
     private lateinit var itemsText: TextView
     private lateinit var imageItem: ImageView
+    private lateinit var progressBar: ProgressBar
     var stoppedFlag: Boolean = false
-
 
 
     @SuppressLint("MissingInflatedId")
@@ -56,37 +57,34 @@ class TourViewActivity(): AppCompatActivity(), OnRobotReadyListener, OnGoToLocat
         locationsText = findViewById<TextView>(R.id.currentLocation)
         itemsText = findViewById<TextView>(R.id.currentItem)
         imageItem = findViewById<ImageView>(R.id.itemImage)
+        progressBar = findViewById<ProgressBar>(R.id.tourProgress)
+        progressBar.max = 10
 
         selectedPlace = intent.getStringExtra("selectedPlace").toString()
 
         isShort = intent.getBooleanExtra("isKurzSelected", false)
-        isLong = intent.getBooleanExtra("isLang",false)
-        isInd = intent.getBooleanExtra("isIndividuell",false)
+        isLong = intent.getBooleanExtra("isLang", false)
+        isInd = intent.getBooleanExtra("isIndividuell", false)
 
 
-        isUndetailed = intent.getBooleanExtra("isEinfachSelected",false)
-        isDetailed = intent.getBooleanExtra("isAusführlichSelected",false)
+        isUndetailed = intent.getBooleanExtra("isEinfachSelected", false)
+        isDetailed = intent.getBooleanExtra("isAusführlichSelected", false)
 
         val selectedLocationsStr = intent.getStringArrayListExtra("selectedLocations")
-
 
 
         val placeHolder = DataLoader.places.filter { dp -> dp.name == selectedPlace }
         indexP = DataLoader.places.indexOf(placeHolder[0])
 
-        Log.i("test" , selectedLocationsStr.toString())
 
         selectedLocations = DataLoader.places[indexP].locations.filter { dl ->
             selectedLocationsStr?.contains(dl.name) == true
         }.toMutableList()
 
-        Log.i("test2" , selectedLocations.toString())
-        Log.i("test3" , isInd.toString())
-
 
         val btnStop = findViewById<Button>(R.id.btnStop)
         btnStop.visibility = Button.GONE
-        findViewById<Button>(R.id.btnStop).setOnClickListener(){
+        findViewById<Button>(R.id.btnStop).setOnClickListener() {
             mRobot.cancelAllTtsRequests()
             mRobot.stopMovement()
             stoppedFlag = true
@@ -95,58 +93,69 @@ class TourViewActivity(): AppCompatActivity(), OnRobotReadyListener, OnGoToLocat
         findViewById<Button>(R.id.btnStart).setOnClickListener {
             btnStop.visibility = Button.VISIBLE
 
-            if(isShort && !isLong){isShort = true}
-            if(!isShort && isLong){isShort = false; isLong = true}
-
-            if(isDetailed && !isUndetailed){isDetailed = true}
-            if(!isDetailed && isUndetailed){isDetailed = false}
-
-            if(isInd){
-                isDetailed = false
-                isShort = false
+            if (isShort && !isLong) {
+                isShort = true
+            }
+            if (!isShort && isLong) {
+                isShort = false; isLong = true
             }
 
+            if (isDetailed && !isUndetailed) {
+                isDetailed = true
+            }
+            if (!isDetailed && isUndetailed) {
+                isDetailed = false
+            }
 
-            if(isShort){
-                mTourManager = TourManager(mRobot,DataLoader.transfers)
-                mTourManager.createShortTour(DataLoader.places[indexP].locations.toMutableList(),isDetailed)
+            if (isInd) {
+                isShort = false
+                isLong = false
+            }
+
+            if (isShort) {
+                mTourManager = TourManager(mRobot, DataLoader.transfers)
+                mTourManager.createShortTour(
+                    DataLoader.places[indexP].locations.toMutableList(),
+                    isDetailed
+                )
                 mTourManager.registerAsTourStopListener { doTourStop() }
-            }else if (isLong){
-                mTourManager = TourManager(mRobot,DataLoader.transfers)
-                mTourManager.createLongTour(DataLoader.places[indexP].locations.toMutableList(),isDetailed)
+                progressBar.max = mTourManager.locationsToVisit.size
+            } else if (isLong) {
+                mTourManager = TourManager(mRobot, DataLoader.transfers)
+                mTourManager.createLongTour(
+                    DataLoader.places[indexP].locations.toMutableList(),
+                    isDetailed
+                )
                 mTourManager.registerAsTourStopListener { doTourStop() }
-            }else if(isInd){
-                mTourManager = TourManager(mRobot,DataLoader.transfers)
+                progressBar.max = mTourManager.locationsToVisit.size
+
+            } else if (isInd) {
+                mTourManager = TourManager(mRobot, DataLoader.transfers)
                 mTourManager.createIndTour(selectedLocations, isDetailed)
                 mTourManager.registerAsTourStopListener { doTourStop() }
+                progressBar.max = mTourManager.locationsToVisit.size
+
             }
         }
 
     }
 
 
-
-    fun doTourStop(){
+    fun doTourStop() {
         thread {
-
-
             runOnUiThread {
                 locationsText.text = mTourManager.currentLocation.name
             }
-
             //speak out every text for the location
             mTourManager.speakTexts(mTourManager.currentLocation.texts)
 
-
-
             sleep(500)
-
-
 
             //speak every location for each item
             mTourManager.currentLocation.items.forEachIndexed { itemIndex, it ->
                 runOnUiThread {
                     itemsText.text = it.name
+                    /*
                     it.texts.forEach { t ->
                         t.mediaUrls.forEach { m ->
                             if (!m.contains("youtube")) {
@@ -154,6 +163,8 @@ class TourViewActivity(): AppCompatActivity(), OnRobotReadyListener, OnGoToLocat
                             }
                         }
                     }
+
+                     */
                 }
                 mTourManager.speakTexts(it.texts)
             }
@@ -161,8 +172,12 @@ class TourViewActivity(): AppCompatActivity(), OnRobotReadyListener, OnGoToLocat
 
 
             Log.i("Arrived", "Bin angekommen")
-            val nextLocationIndex: Int = (mTourManager.locationsToVisit.indexOf(mTourManager.currentLocation)) + 1
+            val nextLocationIndex: Int =
+                (mTourManager.locationsToVisit.indexOf(mTourManager.currentLocation)) + 1
 
+            runOnUiThread {
+                progressBar.progress = nextLocationIndex
+            }
             if (nextLocationIndex < mTourManager.locationsToVisit.size) {
                 val nextLocation: Location = mTourManager.locationsToVisit[nextLocationIndex]
                 Log.i("Next", "Als nächstes $nextLocation")
@@ -176,7 +191,7 @@ class TourViewActivity(): AppCompatActivity(), OnRobotReadyListener, OnGoToLocat
                 transferText.forEach { t -> mTourManager.speakTextsTransfer(t.texts) }
 
                 mTourManager.currentLocation = nextLocation
-            }else if(nextLocationIndex == mTourManager.locationsToVisit.size){
+            } else if (nextLocationIndex == mTourManager.locationsToVisit.size) {
                 mTourManager.speak("Das war es mit der Tour, ich hoffe es hat Ihnen gefallen. Bitte bewerten wie sie die Tour fanden")
                 sleep(2000)
                 val intent = Intent(this, FeedbackActivity::class.java)
@@ -191,6 +206,7 @@ class TourViewActivity(): AppCompatActivity(), OnRobotReadyListener, OnGoToLocat
         super.onStart()
         Robot.getInstance().addOnRobotReadyListener(this)
     }
+
     override fun onStop() {
         super.onStop()
         Robot.getInstance().removeOnRobotReadyListener(this)
@@ -201,11 +217,12 @@ class TourViewActivity(): AppCompatActivity(), OnRobotReadyListener, OnGoToLocat
     }
 
     override fun onRobotReady(isReady: Boolean) {
-        if (isReady){
+        if (isReady) {
             mRobot = Robot.getInstance()
             mRobot.hideTopBar()        // hide top action bar
             // hide pull-down bar
-            val activityInfo: ActivityInfo = packageManager.getActivityInfo(componentName, PackageManager.GET_META_DATA)
+            val activityInfo: ActivityInfo =
+                packageManager.getActivityInfo(componentName, PackageManager.GET_META_DATA)
             Robot.getInstance().onStart(activityInfo)
             mRobot.addOnGoToLocationStatusChangedListener(this)
         }
@@ -219,15 +236,13 @@ class TourViewActivity(): AppCompatActivity(), OnRobotReadyListener, OnGoToLocat
         description: String
     ) {
 
-        if(status == OnGoToLocationStatusChangedListener.ABORT)
-        {
+        if (status == OnGoToLocationStatusChangedListener.ABORT) {
             mRobot.stopMovement()
             mTourManager.speak("Tut mir Leid, es ist wohl ein Fehler aufgetreten, bitte starte die Tour neu")
             sleep(1000)
             mRobot.cancelAllTtsRequests()
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-
 
         }
 
